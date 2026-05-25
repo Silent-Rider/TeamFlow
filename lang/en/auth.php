8c324dca421e5b47ec4c9ecf61afeadaeee02c53
Silent-Rider <silent.30.rider.10@gmail.com>
2026-05-26 01:04:15 +0700

    Добавил локализацию всем окнам авторизации

--- lang/en/auth.php
+++ lang/en/auth.php
@@ -1,20 +1,31 @@
 <?php
 
 return [
-
-    /*
-    |--------------------------------------------------------------------------
-    | Authentication Language Lines
-    |--------------------------------------------------------------------------
-    |
-    | The following language lines are used during authentication for various
-    | messages that we need to display to the user. You are free to modify
-    | these language lines according to your application's requirements.
-    |
-    */
-
     'failed' => 'These credentials do not match our records.',
     'password' => 'The provided password is incorrect.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
 
+    'confirm_password_title' => 'Confirm Password',
+    'confirm_password_desc'  => 'This is a secure area of the application. Please confirm your password before continuing.',
+    'confirm_button'         => 'Confirm',
+
+    'forgot_password_desc'       => 'Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.',
+    'email_label'                => 'Email',
+    'email_reset_link_button'    => 'Email Password Reset Link',
+
+    'password_label'    => 'Password',
+    'remember_me'       => 'Remember me',
+    'forgot_password'   => 'Forgot your password?',
+    'log_in_button'     => 'Log in',
+
+    'name_label'                  => 'Name',
+    'confirm_password_label'      => 'Confirm Password',
+    'already_registered_link'     => 'Already registered?',
+    'register_button'             => 'Register',
+
+    'reset_password_button'       => 'Reset Password',
+    'verify_email_desc'           => 'Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.',
+    'verification_link_sent'      => 'A new verification link has been sent to the email address you provided during registration.',
+    'resend_verification_button'  => 'Resend Verification Email',
+    'log_out_button'              => 'Log Out',
 ];
