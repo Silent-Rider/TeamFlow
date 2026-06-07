363401044feae16eee534a2c992b219a6d8a5cc6
Silent-Rider <silent.30.rider.10@gmail.com>
2026-06-08 01:50:31 +0700

    Улучшил роуты, обьединив их под контроллерами

--- routes/web.php
+++ routes/web.php
@@ -14,12 +14,16 @@ Route::get('/', function () {
 });
 
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::controller(ProfileController::class)->prefix('profile')->group(function () {
+        Route::get('/', 'edit')->name('profile.edit');
+        Route::patch('/', 'update')->name('profile.update');
+        Route::delete('/', 'destroy')->name('profile.destroy');
+    });
 
-    Route::get('/tasks', [TaskController::class, 'index'])->name('tasks');
-    Route::patch('/tasks/{task}/toggle', [TaskController::class, 'toggle'])->name('tasks.toggle');
+    Route::controller(TaskController::class)->prefix('tasks')->group(function () {
+        Route::get('/', 'index')->name('tasks');
+        Route::patch('/{task}/toggle', 'toggle')->name('tasks.toggle');
+    });
 
     Route::get('/projects', [ProjectController::class, 'index'])->name('projects');
 
