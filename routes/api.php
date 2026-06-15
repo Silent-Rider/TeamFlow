1ff7c191cb7f15b4e245097306784651413c5ced
Silent-Rider <silent.30.rider.10@gmail.com>
2026-06-16 01:04:26 +0700

    Лямбда

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,5 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/user', function (Request $request) {
-        return $request->user();
-    });
+    Route::get('/user', fn (Request $r) => $r->user());
 });
