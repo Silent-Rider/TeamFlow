d9fec977c2c20bc44d624d259f649ff02a538111
Silent-Rider <silent.30.rider.10@gmail.com>
2026-07-02 23:47:03 +0700

    Создал собственный пакет avatar-placeholder, запушил на packegist, подключил к проекту.

--- resources/views/components/avatar-placeholder.blade.php
+++
@@ -1,7 +0,0 @@
-<div
-    {{ $attributes->merge([
-        'class' => "flex items-center justify-center rounded-full text-white font-bold select-none",
-        'style' => "background-color: {$backgroundColor};"]) }}
-    aria-label="Avatar for {{ $name }}">
-    {{ $initials }}
-</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ export default {
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+        './vendor/silent_rider/avatar-placeholder/resources/**/*.blade.php',
     ],
 
     theme: {
