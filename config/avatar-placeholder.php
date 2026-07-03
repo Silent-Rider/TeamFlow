0a99db91033841b97a451f950d0030ca8a45a97f
Silent-Rider <silent.30.rider.10@gmail.com>
2026-07-03 14:57:42 +0700

    Добавил конфиг из пакета

---
+++ config/avatar-placeholder.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    'colors' => env('AVATAR_PLACEHOLDER_COLORS', [
+        '#EF4444',
+        '#F97316',
+        '#F59E0B',
+        '#10B981',
+        '#059669',
+        '#14B8A6',
+        '#06B6D4',
+        '#3B82F6',
+        '#6366F1',
+        '#8B5CF6',
+        '#A855F7',
+        '#D946EF',
+        '#EC4899',
+        '#F43F5E'
+    ]),
+    'letters_count' => env('AVATAR_PLACEHOLDER_LETTERS_COUNT', 2),
+];
