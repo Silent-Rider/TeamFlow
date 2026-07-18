a265ff180c848d2a1dfd21acccc79ab7b14b525a
Silent-Rider <silent.30.rider.10@gmail.com>
2026-07-19 02:22:21 +0700

    Добавил прогревку кэша компаний

--- app/Console/Commands/WarmCacheCommand.php
+++ app/Console/Commands/WarmCacheCommand.php
@@ -2,7 +2,9 @@
 
 namespace App\Console\Commands;
 
+use App\Enums\UserRole;
 use App\Models\User;
+use App\Repositories\CompanyRepository;
 use App\Repositories\ProjectRepository;
 use App\Repositories\UserRepository;
 use Illuminate\Console\Attributes\Description;
@@ -16,19 +18,26 @@ class WarmCacheCommand extends Command
     public function __construct(
         private readonly UserRepository $userRepository,
         private readonly ProjectRepository $projectRepository,
+        private readonly CompanyRepository $companyRepository
     ) {
         parent::__construct();
     }
 
     public function handle(): int
     {
-        $users = User::query()->get();
+        $users = User::query()
+            ->where('role', UserRole::USER)
+            ->get();
         $this->withProgressBar($users, function (User $user) {
             $this->userRepository->getUsersDataByCompanyId($user, page: 1, perPage: 50);
             $this->projectRepository->getProjectsDataByUserId($user->id, page: 1, perPage: 20);
         });
         $this->newLine();
         $this->info('Кэш прогрет для ' . $users->count() . ' пользователей.');
+
+        $companies = $this->companyRepository->getCompaniesData(1, 20);
+        $this->newLine();
+        $this->info('Кэш прогрет для ' . count($companies['items']) . ' компаний.');
 
         return self::SUCCESS;
     }
